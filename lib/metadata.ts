99c98fc1ec5702c39cd8f51d641dcc9a75b46c6b
Shiva Yadav <shivaydv1256@gmail.com>
2025-08-03 18:49:09 +0530

    fixed base url

--- lib/metadata.ts
+++ lib/metadata.ts
@@ -25,7 +25,7 @@ export function createMetadata(override: Metadata): Metadata {
 export const baseUrl =
   process.env.NODE_ENV === 'development'
     ? new URL('http://localhost:3000')
-    : new URL(`http://localhost:3000`);
+    : new URL(`https://devaxioms.vercel.app`);
 
 
 export const keywords = ["web development", "interview preparation", "coding interviews", "programming concepts", "software engineering", "technical interviews", "web technologies", "JavaScript", "React", "Node.js", "HTML", "CSS", "Dev Axioms","dev axioms","dev-axioms"];
